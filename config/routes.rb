698a6387e7c635eec41702078a55c7e2126d3d58
 SunScript <sunscript@SunScripts-MacBook-Pro.local>
2020-09-30 23:23:57 +0300

    init basic default cantroller and custom endpoint)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'hello/index'
   resources :cars
+  get 'kurwa' , to: 'cars#kurwa' , as: 'bad_word' 
   root 'hello#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
